5a0ca62bd5947ce0152353af216b7da1ef140dc9
7abf7473 <dllf3lite@gmail.com>
2022-11-26 20:20:50 +0300

    save

--- mutable-webtorrent.ts
+++ mutable-webtorrent.ts
@@ -6,160 +6,171 @@ import sha1 from 'simple-sha1'
 const BTPK_PREFIX = 'urn:btpk:'
 const BITH_PREFIX = 'urn:btih:'
 
-function verify (signature: Buffer, message: Buffer, publicKey: Buffer) {
-  return sodium.crypto_sign_verify_detached(signature, message, publicKey)
+function verify(signature: Buffer, message: Buffer, publicKey: Buffer) {
+    return sodium.crypto_sign_verify_detached(signature, message, publicKey)
 }
 
-function sign (message: Buffer, publicKey: Buffer, secretKey: Buffer) {
-  const signature = Buffer.alloc(sodium.crypto_sign_BYTES)
-  sodium.crypto_sign_detached(signature, message, secretKey)
-  return signature
+function sign(message: Buffer, publicKey: Buffer, secretKey: Buffer) {
+    const signature = Buffer.alloc(sodium.crypto_sign_BYTES)
+    sodium.crypto_sign_detached(signature, message, secretKey)
+    return signature
 }
 
 type u = undefined
-type Callback = (torrent: WebTorrent.Torrent) => any
+type AddMutableOptions = WebTorrent.TorrentOptions
+type AddMutableCallback = (torrent: WebTorrent.Torrent) => any
+type PublishOptions = { sequence: number }
+type Callback<Data = any> = (err?: any, res?: Data) => any
 
 class MutableWebTorrent extends WebTorrent {
-  constructor (options?: WebTorrent.Options) {
-    let dht = options?.dht as (u|{ verify: Function })
-    console.assert(!dht?.verify || dht.verify === verify)
-    super(options)
-  }
-
-  addMutable(magnetURI: string, opts?: WebTorrent.TorrentOptions | Callback, callback: Callback = noop): WebTorrent.Torrent {
-    let options: u|WebTorrent.TorrentOptions
-    if (typeof opts === 'function') {
-        callback = opts
-    } else {
-        options = opts
+    constructor(options?: WebTorrent.Options) {
+        let dht = options?.dht as (u | { verify: Function })
+        console.assert(dht?.verify === verify)
+        super(options)
     }
 
-    const parsed = new URL(magnetURI)
-    const xs = parsed.searchParams.get('xs')
-    const isMutableLink = xs && xs.startsWith(BTPK_PREFIX)
-    if (!isMutableLink) return super.add(magnetURI, options, callback)
-
-    const publicKeyString = xs.slice(BTPK_PREFIX.length)
-
-    this.resolve(publicKeyString, (err, res) => {
-      if (err) return this.emit('error', err)
-      if (!res) return this.emit('error', new Error('Unable to resolve magnet link'))
-
-      const urn = `${BITH_PREFIX}${res.infoHash.toString('hex')}`
-
-      const finalMangetURI = magnetURI + `&xt=${urn}`
-
-      super.add(finalMangetURI, options, (torrent) => {
-        torrent.publicKey = Buffer.from(publicKeyString, 'hex')
-        torrent.sequence = res.sequence || 0
-
-        callback(torrent)
-      })
-    })
-  }
-
-  resolve (publicKeyString, callback) {
-    const publicKey = Buffer.from(publicKeyString, 'hex')
-
-    sha1(publicKey, (targetID) => {
-      this.dht.get(targetID, (err, res) => {
-        if (err) return callback(err)
-
-        try {
-          const infoHash = res.v.ih
-          const sequence = res.seq
-
-          return callback(null, { infoHash, sequence })
-        } catch (parseErr) {
-          return callback(parseErr)
+    addMutable(torrent: any, opts?: AddMutableOptions | AddMutableCallback, callback: AddMutableCallback = noop): void {
+        let options: u | AddMutableOptions
+        if (typeof opts === 'function') {
+            callback = opts
+        } else {
+            options = opts
         }
-      })
-    })
-  }
 
-  publish (publicKeyString: string, secretKeyString: string, infoHashString: string, options, callback) {
-    if (!callback) {
-      callback = options
-      options = { sequence: 1 }
-    } else if (!options) {
-      options = { sequence: 1 }
-    }
-    const buffPubKey = Buffer.from(publicKeyString, 'hex')
-    const buffSecKey = Buffer.from(secretKeyString, 'hex')
-
-    sha1(buffPubKey, (targetID) => {
-      const dht = this.dht
-
-      const opts = {
-        k: buffPubKey,
-        // seq: 0,
-        v: {
-          ih: Buffer.from(infoHashString, 'hex')
-        },
-        sign: (buf) => {
-          return sign(buf, buffPubKey, buffSecKey)
+        if (typeof torrent !== 'string') {
+            super.add(torrent, options, callback)
+            return
         }
-      }
+        let magnetURI = torrent
 
-      dht.get(targetID, (err, res) => {
-        if (err) return callback(err)
+        const parsed = new URL(magnetURI)
+        const xs = parsed.searchParams.get('xs')
+        const isMutableLink = xs && xs.startsWith(BTPK_PREFIX)
+        if (!isMutableLink) {
+            super.add(magnetURI, options, callback)
+            return
+        }
 
-        const sequence = (res && res.seq) ? res.seq + 1 : options.sequence
-        opts.seq = sequence
+        const publicKeyString = xs.slice(BTPK_PREFIX.length)
+        //const publicKey = Buffer.from(publicKeyString, 'hex')
 
-        dht.put(opts, (putErr, hash) => {
-          if (putErr) return callback(putErr)
-
-          const magnetURI = `magnet:?xs=${BTPK_PREFIX}${publicKeyString}`
-
-          callback(null, {
-            magnetURI,
-            infohash: infoHashString,
-            sequence
-          })
+        this.resolve(publicKeyString, (err, res) => {
+            if (err) {
+                return this.emit('error', err)
+            }
+            if (!res) {
+                return this.emit('error', new Error('Unable to resolve magnet link'))
+            }
+            const finalMangetURI = magnetURI + `&xt=${BITH_PREFIX}${res.infoHash.toString('hex')}`
+            super.add(finalMangetURI, options, (torrent) => {
+                //torrent.publicKey = publicKey
+                //torrent.sequence = res.sequence || 0
+                callback(torrent)
+            })
         })
-      })
-    })
-  }
-
-  republish (publicKeyString, callback) {
-    if (!callback) {
-      callback = () => noop
     }
 
-    const buffPubKey = Buffer.from(publicKeyString, 'hex')
-
-    sha1(buffPubKey, (targetID) => {
-      const dht = this.dht
-
-      dht.get(targetID, (err, res) => {
-        if (err) {
-          callback(err)
-          callback = noop
-          return
-        }
-
-        dht.put(res, (err) => {
-          callback(err)
+    resolve(publicKeyString: string, callback: Callback) {
+        let publicKey = Buffer.from(publicKeyString, 'hex')
+        sha1(publicKey, (targetID) => {
+            this.dht.get(targetID, (err, res) => {
+                if (err) {
+                    return callback(err)
+                }
+                let infoHash = res?.v?.ih
+                let sequence = res?.seq
+                if (typeof infoHash === 'string' && typeof sequence === 'number') {
+                    return callback(null, { infoHash, sequence })
+                } else {
+                    //TODO: better error message
+                    return callback(new Error('Unable to parse response'))
+                }
+            })
         })
-      })
-    })
-  }
-
-  createKeypair (seed) {
-    const publicKey = Buffer.alloc(sodium.crypto_sign_PUBLICKEYBYTES)
-    const secretKey = Buffer.alloc(sodium.crypto_sign_SECRETKEYBYTES)
-
-    if (seed) {
-        sodium.crypto_sign_seed_keypair(publicKey, secretKey, seed)
-    } else {
-        sodium.crypto_sign_keypair(publicKey, secretKey)
     }
 
-    return { publicKey: publicKey.toString('hex'), secretKey: secretKey.toString('hex') }
-  }
+    publish(publicKeyString: string, secretKeyString: string, infoHashString: string, opts?: Callback | PublishOptions, callback: Callback = noop) {
+        let options: PublishOptions = { sequence: 1 }
+        if (typeof opts === 'function') {
+            callback = opts
+        } else if(opts !== undefined){
+            options = opts
+        }
+        const buffPubKey = Buffer.from(publicKeyString, 'hex')
+        const buffSecKey = Buffer.from(secretKeyString, 'hex')
+
+        sha1(buffPubKey, (targetID) => {
+            const dht = this.dht
+
+            const opts = {
+                k: buffPubKey,
+                // seq: 0,
+                v: {
+                    ih: Buffer.from(infoHashString, 'hex')
+                },
+                sign: (buf: Buffer) => {
+                    return sign(buf, buffPubKey, buffSecKey)
+                },
+                seq: options.sequence
+            }
+
+            dht.get(targetID, (err, res) => {
+                if (err) {
+                    return callback(err)
+                }
+                let sequence = opts.seq
+                if(res && res.seq) {
+                    sequence = opts.seq = res.seq + 1
+                }
+                dht.put(opts, (putErr, hash) => {
+                    if (putErr) {
+                        return callback(putErr)
+                    }
+                    const magnetURI = `magnet:?xs=${BTPK_PREFIX}${publicKeyString}`
+                    callback(null, {
+                        magnetURI,
+                        infohash: infoHashString,
+                        sequence
+                    })
+                })
+            })
+        })
+    }
+
+    republish(publicKeyString: string, callback: Callback = noop) {
+        const buffPubKey = Buffer.from(publicKeyString, 'hex')
+        sha1(buffPubKey, (targetID) => {
+            const dht = this.dht
+
+            dht.get(targetID, (err, res) => {
+                if (err) {
+                    callback(err)
+                    callback = noop
+                    return
+                }
+
+                dht.put(res, (err) => {
+                    callback(err)
+                })
+            })
+        })
+    }
+
+    createKeypair(seed) {
+        const publicKey = Buffer.alloc(sodium.crypto_sign_PUBLICKEYBYTES)
+        const secretKey = Buffer.alloc(sodium.crypto_sign_SECRETKEYBYTES)
+        if (seed) {
+            sodium.crypto_sign_seed_keypair(publicKey, secretKey, seed)
+        } else {
+            sodium.crypto_sign_keypair(publicKey, secretKey)
+        }
+        return {
+            publicKey: publicKey.toString('hex'),
+            secretKey: secretKey.toString('hex')
+        }
+    }
 }
 
 module.exports = MutableWebTorrent
 
-function noop () {}
+function noop() { }
